22ecf356a2bc26818e0c1a647fe055a2233d8148
Nnenna-Okpe <okpennenna8@gmail.com>
2026-02-13 15:04:17 -0800

    update

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Vals12 from "./components/vals12.jsx";
+import Vals14 from "./components/vals14.jsx";
+import Home from "./components/home.jsx";
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/vals12" element={<Vals12 />} />
+          <Route path="/vals14" element={<Vals14 />} />
+        </Routes>
+      </Router>
+    </>
+  );
+}
+
+export default App;
